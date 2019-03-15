3d6113a836114c82bd12f2082bccbca34927fb39
github username <github email address>
2019-03-15 13:16:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.substring(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^0-9-' ]/g, "")
+  }
+
+  static titleize(string) {
+    return string.split(" ").map((word) => this.capitalize).join(" ");
+  }
 }
