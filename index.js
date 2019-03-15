0266daae193e6fdfb581ba1f12dc9ed0038f2976
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-03-15 13:44:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize(string){
+    let array = string.split(' ')
+    array[0] = Formatter.capitalize(array[0])
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] != 'a' && array[i] != 'the' && array[i] != 'an' && array[i] != 'but' && array[i] != 'of' && array[i] != 'and' && array[i] != 'for' && array[i] != 'at' && array[i] != 'by' && array[i] != 'from'){
+        array[i] = Formatter.capitalize(array[i])
+      }
+    }
+    return array.join(' ')
+  }
 }
