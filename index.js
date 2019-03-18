47ed9503d2ab0c7438b9bfdf2bb30077c08b164e
tiefengeist <sahirnambiar@gmail.com>
2019-03-18 15:40:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    let strnew = str[0].toUpperCase()+str.slice(1);
+    return strnew;
+  }
+  static sanitize(str) {
+    let strnew = str.replace(/[^A-Za-z0-9\s-']+/g, '');
+    return strnew
+  }
+
+  static titleize(str) {
+    let parseString = str.split(' ');
+    let ignoreThese = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    parseString = parseString.map(function(elt, index) {
+      if (index == 0) return elt[0].toUpperCase() + elt.slice(1);
+      else if (ignoreThese.includes(elt)) return elt;
+      else return elt[0].toUpperCase() + elt.slice(1);
+    }).join(' ')
+    return parseString;
+  }
 }
