3e198b65e4c074da8f0e762dacdcb0831d14a923
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-03-17 19:27:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    let newStr = str[0].toUpperCase() + str.slice(1);
+    return newStr;
+  }
+
+  static sanitize(str) {
+    let newStr = str.replace(/[^A-Za-z0-9\s-']+/g, '');
+    return newStr
+  }
+
+  static titleize(str) {
+    let newStr = str.split(' ');
+    let nahWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    newStr = newStr.map(function(elmt, index) {
+      if (index == 0) return elmt[0].toUpperCase() + elmt.slice(1);
+      else if (nahWords.includes(elmt)) return elmt;
+      else return elmt[0].toUpperCase() + elmt.slice(1);
+    })
+      .join(' ')
+
+    return newStr;
+  }
 }
